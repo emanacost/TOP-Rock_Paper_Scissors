ba0bbea6063a30c320bbc10af94ca5f548b4c40c
eacosta <eacosta0411@gmail.com>
2025-05-15 19:45:06 -0400

    Make a change to the comment to reflect what it is now commenting on

--- Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors.js
@@ -4,10 +4,9 @@
 //Simple test to make sure JS is linked to html
 console.log("I'm Alive!")
 
-// Pseudocode for the program
+// Pseudocode for the random number function
 
 // Write a function that randomly returns Rock Papers or Scissors
     // Initialize function Return R, P, or S
     // Randomly generate a number
     // Depending on the number return R, P, or S
-
