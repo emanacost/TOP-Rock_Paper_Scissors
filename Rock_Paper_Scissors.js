a878ad9d270594de15cb8e38ac8b54f29aba55ea
eacosta <eacosta0411@gmail.com>
2025-05-15 19:29:43 -0400

    add JavaScript file for Rock Paper Scissors game logic

---
+++ Rock_Paper_Scissors.js
@@ -0,0 +1,5 @@
+// A Rock Papers Scissors game written in Javascript
+// No UI, will just use console and browser text prompt
+
+//Simple test to make sure JS is linked to html
+console.log("I'm Alive!")
